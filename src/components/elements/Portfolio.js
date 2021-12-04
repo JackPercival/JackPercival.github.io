6745ec7392f3195beaabe7401f8f825f274ce621
Jack Percival <jackpercival7@gmail.com>
2021-12-04 09:20:55 -0800

    Added a lot

--- src/components/elements/Portfolio.js
+++ src/components/elements/Portfolio.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import FsLightbox from "fslightbox-react";
 
 function Portfolio({ portfolio }) {
-  const { category, title, description, image, popupLink, link } = portfolio;
+  const { title, shortDescription, bullet1, bullet2, bullet3, image, popupLink, link } = portfolio;
   const [toggler, setToggler] = useState(false);
 
   const handleLightbox = (e) => {
@@ -12,43 +12,44 @@ function Portfolio({ portfolio }) {
     }
   };
 
-  const handleIcon = () => {
-    if (link) {
-      return <i className="icon-link"></i>;
-    } else if (popupLink) {
-      if (popupLink.length > 1) {
-        if (popupLink.toString().match(/youtube/g)) {
-          return <i className="icon-camrecorder"></i>;
-        }
-        return <i className="icon-picture"></i>;
-      } else if (popupLink.toString().match(/youtube/g)) {
-        return <i className="icon-camrecorder"></i>;
-      } else {
-        return <i className="icon-magnifier-add"></i>;
-      }
-    }
-    return <i className="icon-magnifier-add"></i>;
-  };
   return (
     <>
       <a
         href={link ? link : "!#"}
         className="work-image"
         onClick={handleLightbox}
+        target="_blank"
       >
         <div className="portfolio-item rounded shadow-dark">
           <div className="details">
             {/* <span className="term text-capitalize">{category}</span> */}
             <h4 >{title}</h4>
-            <h4 id="projectDescription">{description}</h4>
-            <span className="more-button">{handleIcon()}</span>
+            <span className="more-button"><i className="icon-link"></i></span>
           </div>
           <div className="thumb">
-            <img src={image} alt="Portfolio-title" />
+            <img src={image} alt="Portfolio-title" className="projectImage"/>
             <div className="mask"></div>
           </div>
         </div>
       </a>
+      <div className="projectInfo">
+        <div className="projectNameAndLinks">
+          <h4>{title}</h4>
+          <div className="projectLinks">
+            <a href={link} target="_blank">Live site</a>
+            <div className="dotBetweenLinks">•</div>
+            <li className="list-inline-item">
+              <a href="https://github.com/JackPercival" target="_blank">
+                <i className="fab fa-github"></i>
+              </a>
+            </li>
+          </div>
+        </div>
+        <h5>{shortDescription}</h5>
+        <p>{bullet1}</p>
+        <p>{bullet2}</p>
+        <p>{bullet3}</p>
+      </div>
       {popupLink && <FsLightbox toggler={toggler} sources={popupLink} />}
     </>
   );
